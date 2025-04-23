4685ea87324df8df508a1fae6bbdb5b894329ea0
Moideensha CP <your-email@example.com>
2025-04-23 10:41:14 +0530

    changed the pagination listing

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -696,7 +696,7 @@ const editProduct = async (req, res) => {
 const loadOrder = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 3;
+    const limit = parseInt(req.query.limit) || 5;
     const skip = (page - 1) * limit;
     const orderlist = await Order.find()
       .populate({
